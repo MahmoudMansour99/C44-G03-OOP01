23eee20dad31107f66810deba8127c772b06fb4c
Mahmoud Mansour <mansour@gmail.com>
2025-07-25 20:15:40 +0300

    Complete 3rd Video - Part 07 Access Modifiers

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,12 +7,23 @@ namespace Demo
         static void Main(string[] args)
         {
             #region Class Library
-            Product product = new Product();
+
+            //Product product = new Product();
             #endregion
 
             #region User Defined Data Type
 
 
+
+            #endregion
+
+            #region Access Modifier 
+            TypeA typeA = new TypeA();
+            //typeA.X = 10; // Invalid because it's private and in another project 
+
+            //typeA.Y = 10; // Invalid because its internal and in another prohject
+
+            //typeA.Z = 10; // Valid because it is public 
 
             #endregion
         }
