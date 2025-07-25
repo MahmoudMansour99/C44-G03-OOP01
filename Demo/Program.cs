4916b7d3a51cb9dd7548e0704fb92f2971235a15
Mahmoud Mansour <mansour@gmail.com>
2025-07-25 20:49:50 +0300

    Complete 4th Video - Part 08 Enum Example 01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -26,6 +26,38 @@ namespace Demo
             //typeA.Z = 10; // Valid because it is public 
 
             #endregion
+
+            #region Enums
+
+            #region Example 01
+            //Person person = new Person();
+
+            //person.Id = 10;
+            //person.Name = "Ahmed";
+            ////person.Gender = "Hamada";
+            //person.Gender = Gender.Male;
+
+            //Grade G01 = Grade.A;
+            //Grade G01 = Grade.B;
+
+            //if (G01 == Grade.A)
+            //    Console.WriteLine("Barvo!");
+            //else
+            //    Console.WriteLine(":(");
+
+            //Grade G01 = (Grade)10;
+
+            //Console.WriteLine(G01);
+
+            //Grade G01 = new Grade();
+
+            //Console.WriteLine(G01);
+
+            #endregion
+
+
+
+            #endregion
         }
     }
 }
