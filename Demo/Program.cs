17a9130825d3842c4af7e26f0f5049a194b5679f
Mahmoud Mansour <mansour@gmail.com>
2025-07-26 06:51:24 +0300

    Complete 6th Video - Part 01 Enum Example 03 Permissions

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -66,57 +66,106 @@ namespace Demo
             //    Grade = Grade.B,
             //};
 
-            Student student = new Student();
-            bool IsParse;
-            Console.WriteLine("Please Enter Student Data: - ");
-            int Id;
+            //Student student = new Student();
+            //bool IsParse;
+            //Console.WriteLine("Please Enter Student Data: - ");
+            //int Id;
             
-            do
-            {
-                Console.Write("Id: ");
-                IsParse = int.TryParse(Console.ReadLine(), out Id);
-            }
-            while (!IsParse);
+            //do
+            //{
+            //    Console.Write("Id: ");
+            //    IsParse = int.TryParse(Console.ReadLine(), out Id);
+            //}
+            //while (!IsParse);
 
-            student.Id = Id; // Student Id
+            //student.Id = Id; // Student Id
 
-            Console.Write("Name: ");
-            student.Name = Console.ReadLine(); // Student Name
+            //Console.Write("Name: ");
+            //student.Name = Console.ReadLine(); // Student Name
 
-            object Gender;
+            //object Gender;
 
-            do
-            {
-                Console.Write("Gender: ");
-                IsParse = Enum.TryParse(typeof(Gender) ,Console.ReadLine(), out Gender);
-            }
-            while (!IsParse || Gender == null);
+            //do
+            //{
+            //    Console.Write("Gender: ");
+            //    IsParse = Enum.TryParse(typeof(Gender) ,Console.ReadLine(), out Gender);
+            //}
+            //while (!IsParse || Gender == null);
 
-            student.Gender = (Gender)Gender;
+            //student.Gender = (Gender)Gender;
 
-            Branch Branch;
+            //Branch Branch;
 
-            do
-            {
-                Console.Write("Branch: ");
-                IsParse = Enum.TryParse<Branch>(Console.ReadLine(), out Branch);
-            }
-            while (!IsParse);
+            //do
+            //{
+            //    Console.Write("Branch: ");
+            //    IsParse = Enum.TryParse<Branch>(Console.ReadLine(), out Branch);
+            //}
+            //while (!IsParse);
 
-            student.Branch = Branch;
+            //student.Branch = Branch;
 
-            Grade Grade;
+            //Grade Grade;
 
-            do
-            {
-                Console.Write("Grade: ");
-                IsParse = Enum.TryParse(Console.ReadLine(), out Grade);
-            }
-            while (!IsParse);
-            student.Grade = Grade;
+            //do
+            //{
+            //    Console.Write("Grade: ");
+            //    IsParse = Enum.TryParse(Console.ReadLine(), out Grade);
+            //}
+            //while (!IsParse);
+            //student.Grade = Grade;
 
-            Console.Clear();
-            Console.WriteLine($"Hello {student.Name} Welcome to Route Academy\n Branch is {student.Branch} and your Grade is {student.Grade}");
+            //Console.Clear();
+            //Console.WriteLine($"Hello {student.Name} Welcome to Route Academy\n Branch is {student.Branch} and your Grade is {student.Grade}");
+
+            #endregion
+
+            #region Example 03
+
+            //User user01 = new User();
+
+            //user01.Id = 10;
+
+            ////user01.Permessions[0] = true; // Write
+            ////user01.Permessions[0] = false; // Read
+            ////user01.Permessions[0] = true; // Execute
+            ////user01.Permessions[0] = false; // Delete
+
+            //user01.Permissions = (Permissions)3;
+            ////                        0 0 1 1   Delete & Execute    
+
+            //user01.Permissions = user01.Permissions ^ Permissions.Read;
+            //                           3          ^      4
+            //                        0011          ^     0100          = 0111 => 4 + 2 + 1 = 7  
+
+            //Console.WriteLine(user01.Permissions);
+
+            ////user01.Permissions = user01.Permissions ^ Permissions.Read;
+            ////                           7          ^      4
+            ////                        0111          ^     0100          = 0011 => 2 + 1 = 3  
+
+            ////Console.WriteLine(user01.Permissions);
+
+            //user01.Permissions = user01.Permissions & Permissions.Read;
+            ////                           3          ^      4
+            ////                        0111          ^     0100          = 0011 => 2 + 1 = 3  
+
+            //Console.WriteLine(user01.Permissions);
+
+            //if ((user01.Permissions & Permissions.Execute) == Permissions.Execute)
+            //    Console.WriteLine("User has execute Permessions");
+            //else
+            //    user01.Permissions = user01.Permissions ^ Permissions.Execute;
+
+            //user01.Permissions |= Permissions.Read;
+
+            //if ((user01.Permissions | Permissions.Read) == Permissions.Read)
+            //    Console.WriteLine("User has Read Permessions");
+            //else
+            //    user01.Permissions = user01.Permissions ^ Permissions.Read;
+
+            //Console.WriteLine(user01.Permissions);
+
 
             #endregion
 
